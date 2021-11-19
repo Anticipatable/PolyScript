c965deb6378de079e6f228d13c5fb2ba6e15d32b
Levi <64054647+Anticipatable@users.noreply.github.com>
2021-11-18 23:24:04 -0500

    Update AutoFavoriteItems.js

--- AutoFavoriteItems.js
+++ AutoFavoriteItems.js
@@ -3,7 +3,7 @@ function randomIntFromInterval(min, max) { // min and max included
 }
 
 setInterval(function(){ 
-  $.post("/api/catalog/favourite", {id: randomIntFromInterval(5000, 10000), csrf: "46021fe08bc7a45f301316d8dfafe76ece3c34b84f20424d0eb0160e46f7992d"}, function (data) {
+  $.post("/api/catalog/favourite", {id: randomIntFromInterval(5000, 10000), csrf: "your csrf token"}, function (data) {
     if (data == "success") {
       LoadFavourites();
     } else {
