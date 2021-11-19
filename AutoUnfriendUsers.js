ebe55c2dba02108cc91521a52994be226d95825e
Levi <64054647+Anticipatable@users.noreply.github.com>
2021-11-18 23:25:10 -0500

    Create AutoUnfriendUsers.js

---
+++ AutoUnfriendUsers.js
@@ -0,0 +1,13 @@
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+setInterval(function(){ 
+    $.post("/api/users/removefriend", { "csrf" : "your csrf token", "id": randomIntFromInterval(1, 157030)}, function(data) {
+      if (data == "success") {
+       //LoadFriendButton();
+      } else {
+        console.log("Error")
+        //alert(data);
+      }
+    });
+}, 100);
