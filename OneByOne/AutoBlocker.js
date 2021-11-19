eae145494901861b979f1be4aacb47403de3601e
Levi <64054647+Anticipatable@users.noreply.github.com>
2021-11-19 06:48:06 -0500

    Update AutoBlocker.js

--- OneByOne/AutoBlocker.js
+++ OneByOne/AutoBlocker.js
@@ -1,9 +1,9 @@
 num = 1
 
 setInterval(function(){ 
-    $.post("/api/users/block", {id: randomIntFromInterval(1, 157030), csrf: "your csrf token"}, function (data) {
+    $.post("/api/users/block", {id: num, csrf: "your csrf token"}, function (data) {
       if (data == "blocked") {
-num=num+1
+        num=num+1
         //$("#block-user").html("Unblock");
       } else {
         num=num+1
