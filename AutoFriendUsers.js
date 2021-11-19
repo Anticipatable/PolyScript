9b1d7bf69abf07227b027d60077ab7d02aa2ee6c
Levi <64054647+Anticipatable@users.noreply.github.com>
2021-11-18 23:24:29 -0500

    Update AutoFriendUsers.js

--- AutoFriendUsers.js
+++ AutoFriendUsers.js
@@ -2,7 +2,7 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 setInterval(function(){ 
-    $.post("/api/users/addfriend", { "csrf" : "46021fe08bc7a45f301316d8dfafe76ece3c34b84f20424d0eb0160e46f7992d", "id": randomIntFromInterval(1, 157030)}, function(data) {
+    $.post("/api/users/addfriend", { "csrf" : "your csrf token", "id": randomIntFromInterval(1, 157030)}, function(data) {
       if (data == "success") {
        //LoadFriendButton();
       } else {
