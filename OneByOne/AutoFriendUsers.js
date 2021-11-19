9ca8d129e5e8374b31714b4350a71d5c99c58eda
Levi <64054647+Anticipatable@users.noreply.github.com>
2021-11-18 23:37:39 -0500

    Create AutoFriendUsers.js

---
+++ OneByOne/AutoFriendUsers.js
@@ -0,0 +1,14 @@
+var num = 1
+
+setInterval(function(){ 
+    $.post("/api/users/addfriend", { "csrf" : "your csrf token", "id": num}, function(data) {
+      if (data == "success") {
+        num=num+1
+       //LoadFriendButton();
+      } else {
+        num=num+1
+        console.log("Error")
+        //alert(data);
+      }
+    });
+}, 100);
