e6dd9484c18693509693426299c24dbe209bbeab
Shiggy <48862659+Shigetorum635@users.noreply.github.com>
2021-11-23 19:10:38 +0100

    Proper Friending
    - Remove JQuery
    - Remove old ES syntax
    - Introduce more dynamic usage
    - Asynchronous Usage
    
    Thought you'd like it ;)

--- OneByOne/AutoFriendUsers.js
+++ OneByOne/AutoFriendUsers.js
@@ -1,13 +1,27 @@
-var num = 1
+const friendTo = 17000
+const csrf = "idk";
 
-setInterval(function(){ 
-    $.post("/api/users/addfriend", { "csrf" : "your csrf token", "id": num}, function(data) {
-      if (data == "success") {
-        num=num+1
-       //LoadFriendButton();
-      } else {
-        num=num+1
-        console.log(`Error - ${data}`)
-      }
-    });
-}, 100);
+async function friend(id){
+await fetch("https://polytoria.com/api/users/addfriend", {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Requested-With": "XMLHttpRequest",
+        "Alt-Used": "polytoria.com",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin"
+    },
+    "referrer": "https://polytoria.com/user/17978",
+    "body": `csrf=${csrf}&id=${id}`,
+    "method": "POST",
+    "mode": "cors"
+});
+}
+
+for ( let iterator = 0; iterator<friendTo; iterator++) {
+  friend(iterator).then((finished) => console.log("Friended %d", iterator));
+}
