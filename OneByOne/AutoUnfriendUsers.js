7e94aa32a0d5e58fd910e00b9cbc0a335cd9a4d6
xenial <64054647+xenially@users.noreply.github.com>
2022-01-10 09:21:13 -0500

    Proper Unfriending (#1)
    Thx Shiggy

--- OneByOne/AutoUnfriendUsers.js
+++ OneByOne/AutoUnfriendUsers.js
@@ -1,12 +1,27 @@
-num = 1
+const friendTo = 17000
+const csrf = "idk";
 
-setInterval(function(){ 
-    $.post("/api/users/removefriend", { "csrf" : "your csrf token", "id": num}, function(data) {
-      if (data == "success") {
-        num=num+1
-      } else {
-        num=num+1
-        console.log(`Error - ${data}`)
-      }
-    });
-}, 100);
+async function unfriend(id){
+await fetch("https://polytoria.com/api/users/removefriend", {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Requested-With": "XMLHttpRequest",
+        "Alt-Used": "polytoria.com",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin"
+    },
+    "referrer": "https://polytoria.com/user/17978",
+    "body": `csrf=${csrf}&id=${id}`,
+    "method": "POST",
+    "mode": "cors"
+});
+}
+
+for ( let iterator = 0; iterator<friendTo; iterator++) {
+  unfriend(iterator).then((finished) => console.log("Unfriended %d", iterator));
+}
